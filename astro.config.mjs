2811fbedd7c42ea5189399f28f6d921fa5424e7e
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-09-27 01:58:11 -0300

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
