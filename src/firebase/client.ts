c871b8ae84d1c4e20554b5393b65bbeaac3eba9e
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-10-04 00:46:22 -0300

    Agrega lógica para subir foto a firebase y mandar información a través del endpoint del backend

---
+++ src/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXhomTJ_fh-3f4dxDd1xNXaDa2_4-VtK8",
+  authDomain: "cuarto-detonao.firebaseapp.com",
+  projectId: "cuarto-detonao",
+  storageBucket: "cuarto-detonao.appspot.com",
+  messagingSenderId: "766950789836",
+  appId: "1:766950789836:web:9e5147979c1d7905db2869"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
